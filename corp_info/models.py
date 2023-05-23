6dbd835a8876010bb7335530ba06715c9d0c736b
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-05-24 00:26:58 +0900

    D-01 , 기업 정보 저장 GET API

--- corp_info/models.py
+++ corp_info/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-# todo
-"""
-    1. information 생성
-        - 기업명
-        - 대표이사
-        - 재무제표
-        -     
-"""
+
+class Corporation(models.Model):
+    corp_name = models.CharField(max_length=100)
+    ceo_name = models.CharField(max_length=100)
+    corp_addr = models.CharField(max_length=100)
+    corp_homepage = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    est_date = models.CharField(max_length=100)
+    sales_revenue = models.CharField(max_length=100)
+    operating_profit = models.CharField(max_length=100)
